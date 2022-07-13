9c46892b3ed686a925d150a9d40212200747ef5f
Rebecca Sissman <rsissman15@aol.com>
2022-07-13 12:31:26 -0400

    Made Updates

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,27 +2,15 @@ class JobsController < ApplicationController
 
     get '/jobs' do 
         jobs=Job.all
-        jobs.to_json
-    end
-
-    get '/jobsandcompanies' do 
-        jobs=Job.all
         jobs.to_json(include: [:company])
     end
 
+   
     get '/jobs/:id' do 
         job=Job.find(params[:id])
         job.to_json
     end
 
-    # post "/jobs" do 
-    #     job= Job.new(params[:job])
-    #     if job.save
-    #         job.to_json
-    #     else
-    #         {errors: job.errors.full_messages}.to_json
-    #     end
-    # end
 
     post "/jobs" do
         job=Job.create(
@@ -42,7 +30,6 @@ class JobsController < ApplicationController
 
 
     patch '/jobs/:id' do
-        binding.pry
         job = Job.find(params[:id])
         job.update(
             interview_round: params[:interview_round]
